cae16ebeb0eac7b447cb68e86347f38c94926eda
Arakawa Shintaro <arashinbe@gmail.com>
2016-03-10 22:06:41 +0900

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  class ProductsController < ApplicationController
+
+    def index
+      @products = Product.all
+      render json: @products
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resources :products
+    end
+    namespace :v2 do
+      resources :products
+    end
+  end
+
+  resources :products
+  root to: 'products#index'
+end
